56026db151dfb4d4eb5d74a28c2bdbca99ced2c1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:27:27 +0100

    Update exports.swift with full @_exported re-exports
    Re-export all semantic dependencies for consumers.

--- Sources/Vector Primitives/exports.swift
+++ Sources/Vector Primitives/exports.swift
@@ -1,4 +1,8 @@
 // exports.swift
-// Re-export dependencies so consumers get Affine types automatically
+// Re-export dependencies for consumers.
 
 @_exported import Affine_Primitives
+@_exported import Bit_Primitives
+@_exported import Finite_Primitives
+@_exported import Index_Primitives
+@_exported import Ordinal_Primitives
